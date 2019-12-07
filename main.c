ed6460508bf8e12403008b5ac8ad9c88d18e727e
vapvin <tpehdj12@daum.net>
2019-12-07 18:42:12 +0900

    This C Language Input and Output

--- main.c
+++ main.c
@@ -1,9 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void) {
 
-    int a = 7;
+int a;
+
+int main(void) {
+    
+    scanf("%d", &a);
     printf("The Number is %d \n", a);
     system("pause");
     return 0;
