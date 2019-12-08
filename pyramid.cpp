54402709aa5e87659034849485a0b1828a931e29
vapvin <tpehdj12@daum.net>
2019-12-08 13:47:55 +0900

    Down Star

--- pyramid.cpp
+++ pyramid.cpp
@@ -10,5 +10,12 @@ int main(void) {
         printf("\n");
     }
 
+    for (int i = n -1; i > 0; i--) {
+        for (int j = 0; j < i; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
     return 0;
 }
