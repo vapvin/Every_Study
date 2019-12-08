8f7ee125732ebd79e603953b8984e1c2126359b9
vapvin <tpehdj12@daum.net>
2019-12-08 10:52:39 +0900

    This C!

--- main.c
+++ main.c
@@ -1,17 +1,49 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
+#define SIZE 10000
+#define INF 999999
 
-int main(void) {
-    
-    int a[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+int queue[SIZE];
+int front = 0;
+int rear = 0;
 
-    int *b = a;
+void push(int data) {
+    if(rear >= SIZE) {
+        printf("큐 오버플로우가 발생했습니다. \n");
+        return;
+    }
 
-    printf("%d \n", b[4]); 
+    queue[rear++] = data;
+}
+
+void pop() {
+    if (front == rear) {
+        printf("큐 언더플로우가 발생했습니다. \n");
+        return -INF;
+    }
+
+    return queue[front++];
+
+}
+
+
+void show() {
+    printf("--- 큐의 앞 ---\n");
+    for (int i = front; i < rear; i++) {
+        printf("%d\n", queue[i]);
+    }
+    printf("--- 큐의 뒤 ---\n");
+}   
+
+int main(void){
+    push(7);
+    push(6);
+    push(5);
+    pop(7);
+    push(4);
+    pop(6);
 
     system("pause");
 
     return 0;
-    
 }
