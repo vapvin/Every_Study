e44edd52095d557e027cee5eec0903b556fc3a19
vapvin <tpehdj12@daum.net>
2019-12-08 11:14:39 +0900

    Complete Queue

--- main.c
+++ main.c
@@ -27,7 +27,42 @@ void push(Queue *queue, int data) {
     queue -> count++;
 }
 
+int pop(Queue *queue) {
+    if(queue -> count == 0) {
+        printf("큐 언더플로우가 발생했습니다. \n");
+        return -INF;
+    }
+    Node *node = queue -> front;
+    int data = node -> data;
+    queue -> front = node -> next;
+    free(node);
+    queue -> count--;
+    return data;
+}
+
+void show(Queue *queue) {
+    Node *cur = queue -> front;
+    printf("--- 큐의 앞 ---\n");
+    while (cur != NULL) {
+        printf("%d\n", cur -> data);
+        cur = cur -> next;
+    }
+    printf("--- 큐의 뒤 ---\n");
+}
+
 int main(void){
+
+    Queue queue;
+    queue.front = queue.rear = NULL;
+    queue.count = 0;
+    push(&queue, 7);
+    push(&queue, 6);
+    push(&queue, 4);
+    pop(&queue);
+    push(&queue, 8);
+    push(&queue, 9);
+    pop(&queue);
+    show(&queue);
   
     system("pause");
 
