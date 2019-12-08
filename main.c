4d01177133784adae7b993559e244aa36c01dece
vapvin <tpehdj12@daum.net>
2019-12-08 10:39:19 +0900

    C Language Pointer 02

--- main.c
+++ main.c
@@ -4,13 +4,13 @@
 
 int main(void) {
     
-    int a[] = {1,2,3,4,5,6,7,8,9,10};
+    int a = 5;
 
-    int i;
+    int *b = &a;
 
-    for (i = 0; i < 10; i++ ) {
-        printf("%d \n", &a[i]);
-    }
+    int **c = &b;
+
+    printf("%d", **c);
 
     system("pause");
 
